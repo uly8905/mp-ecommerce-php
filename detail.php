efd47392845c913ff8f2be16ad5bba874aad5e58
Ulises Sosa <ush.sosa@gmail.com>
2020-10-30 16:52:06 -0500

    remove space

--- detail.php
+++ detail.php
@@ -45,7 +45,7 @@
             'number'=>'5549737300'
         ],
         'address'=>[
-            'street_name'=>' Insurgentes Sur',
+            'street_name'=>'Insurgentes Sur',
             'street_number'=>'1602',
             'zip_code'=>'03940'
             ]
