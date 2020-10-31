16263c9716943460889b4cb9d9db15c891d65c19
Ulises Sosa <ush.sosa@gmail.com>
2020-10-31 00:24:48 -0500

    only json

--- ipn.php
+++ ipn.php
@@ -8,38 +8,38 @@ if($json = json_decode(file_get_contents("php://input"), true)) {
 }
 
 
-MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
-try{
-switch($json['type']) {
-    case "payment":
-        $payment = MercadoPago\Payment.find_by_id($json->data->id);
-        break;
-    case "plan":
-        $plan = MercadoPago\Plan.find_by_id($json->data->id);
-        break;
-    case "subscription":
-        $plan = MercadoPago\Subscription.find_by_id($json->data->id);
-        break;
-    case "invoice":
-        $plan = MercadoPago\Invoice.find_by_id($json->data->id);
-        break;
-}
+// MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
+// // try{
+// switch($json['type']) {
+//     case "payment":
+//         $payment = MercadoPago\Payment.find_by_id($json->data->id);
+//         break;
+//     case "plan":
+//         $plan = MercadoPago\Plan.find_by_id($json->data->id);
+//         break;
+//     case "subscription":
+//         $plan = MercadoPago\Subscription.find_by_id($json->data->id);
+//         break;
+//     case "invoice":
+//         $plan = MercadoPago\Invoice.find_by_id($json->data->id);
+//         break;
+//     }
     // session_start();
-    $msg="";
-    if(isset($payment))
-    {
-        // $_SESSION["notificacion_pay"]=$payment;
-        $msg = json_encode($payment);
-    }
-    if(isset($plan))
-    {
-        // $_SESSION["notificacion_plan"]=$plan;
-        $msg = json_encode($plan);
-    }
-}catch(\Exception $ex)
-{
-    $msg=$ex;
-}
+    // $msg="";
+    // if(isset($payment))
+    // {
+    //     // $_SESSION["notificacion_pay"]=$payment;
+    //     $msg = json_encode($payment);
+    // }
+    // if(isset($plan))
+    // {
+    //     // $_SESSION["notificacion_plan"]=$plan;
+    //     $msg = json_encode($plan);
+    // }
+// }catch(\Exception $ex)
+// {
+//     $msg=$ex;
+// }
 
     $transport = (new \Swift_SmtpTransport('smtp.gmail.com', 465, 'SSL'))
         ->setUsername('ush.sosa@gmail.com')
@@ -54,7 +54,7 @@ switch($json['type']) {
         $message = (new \Swift_Message('notificacion mercadopago'))
         ->setFrom(['ush.sosa@gmail.com'=>'Ulises Sosa'])
         ->setTo('ush.sosa@gmail.com')
-        ->setBody($msg.'</br>inicia bloque 2 </br>'.$json_encode,'text/html');
+        ->setBody($json_encode,'text/html');
 
         // Send the message
         $result = $mailer->send($message);
